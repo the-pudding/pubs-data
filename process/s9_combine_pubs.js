1b963ac789c736210bb41924dddbc54c9a5f2065
Jan Diehm <jadiehm@gmail.com>
2019-06-18 14:25:38 -0500

    combined csv

---
+++ process/s9_combine_pubs.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const d3 = require('d3');
+const _ = require('lodash');
+
+const OUT_PATH = './output/'
+const IN_PATH = './output/pubAddresses/'
+let combinedPubs = []
+
+function processCSV(filename) {
+	console.log(filename)
+	let raw = fs.readFileSync(`${IN_PATH}${filename}`, 'utf-8')
+	let csv = d3.csvParse(raw)
+	let newdb = _.unionBy(combinedPubs, csv, 'pubName')
+	combinedPubs = newdb
+}
+
+function init() {
+	const files = fs.readdirSync(IN_PATH).filter(d => d.includes('.csv'));
+
+	_.each(files, filename => processCSV(filename))
+	const all = d3.csvFormat(combinedPubs)
+	fs.writeFileSync(`${OUT_PATH}/combinedPubs.csv`, all)
+}
+
+init();
