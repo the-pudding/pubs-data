e72860633bda1b01b2cf2e2e3a68e315dd8cfbc2
Jan Diehm <jadiehm@gmail.com>
2019-09-23 16:17:51 -0500

    routes files to individual pub names

--- process/coordinates/s1_combine_25plus.js
+++ process/coordinates/s1_combine_25plus.js
@@ -6,10 +6,7 @@ const OUT_PATH = './output/over-25/results-combined/'
 const IN_PATH = './output/over-25/results/'
 let PUB_NAMES = [];
 let GROUPED_FILES = [];
-let GROUPED_COORDS = [];
 let FLAT_COORDS = [];
-let GROUPED_DIST = [];
-let GROUPED_DUR = [];
 let SUMMED_DIST = [];
 let SUMMED_DUR = [];
 
@@ -35,38 +32,8 @@ function stripFilename(filename) {
 	PUB_NAMES = _.uniqBy(PUB_NAMES, 'stripped')
 }
 
-function loadGroupedFiles(file) {
-	let filePath = `${IN_PATH}${file}`
-	let raw = fs.readFileSync(`${filePath}`, 'utf-8')
-	let parsedData = JSON.parse(raw)
-	let rawCoordinates = parsedData.routes[0].geometry.coordinates
-	let rawDuration = parsedData.routes[0].duration
-	let rawDistance = parsedData.routes[0].distance
-
-	if (!filePath.includes('-1')) {
-		rawCoordinates = rawCoordinates.slice(0, -1)
-	}
-
-	GROUPED_COORDS.push(rawCoordinates)
-	GROUPED_DUR.push(rawDuration)
-	GROUPED_DIST.push(rawDistance)
-}
-
-function combineFiles(strippedName) {
-	let pub = strippedName.stripped
-
-	GROUPED_FILES = fs.readdirSync(IN_PATH).filter(d => d.includes(pub))
-	GROUPED_FILES.map(loadGroupedFiles)
-}
-
-function combineMetrics() {
-	FLAT_COORDS = [].concat.apply([], GROUPED_COORDS)
-	SUMMED_DUR = GROUPED_DUR.reduce((a,b) => a + b, 0)
-	SUMMED_DIST = GROUPED_DIST.reduce((a,b) => a + b, 0)
-}
-
-function replaceData(strippedName) {
-	let pub = strippedName.stripped
+function replaceData(pubName) {
+	let pub = pubName
 	let filePath = `${IN_PATH}result-coordinates-${pub}-1.txt`
 	let raw = fs.readFileSync(`${filePath}`, 'utf-8')
 	let parsedData = JSON.parse(raw)
@@ -80,14 +47,78 @@ function replaceData(strippedName) {
 	fs.writeFileSync(fileOut, output)
 }
 
+function combineMetrics(GROUPED_COORDS, GROUPED_DUR, GROUPED_DIST) {
+	FLAT_COORDS = [].concat.apply([], GROUPED_COORDS)
+	SUMMED_DUR = GROUPED_DUR.reduce((a,b) => a + b, 0)
+	SUMMED_DIST = GROUPED_DIST.reduce((a,b) => a + b, 0)
+}
+
+function loadSingleFile(file) {
+	let pub = file.split('-')[2]
+	let filePath = `${IN_PATH}${file}`
+	let raw = fs.readFileSync(`${filePath}`, 'utf-8')
+	let parsedData = JSON.parse(raw)
+	let rawCoordinates = parsedData.routes[0].geometry.coordinates
+	let rawDuration = parsedData.routes[0].duration
+	let rawDistance = parsedData.routes[0].distance
+
+	if (!filePath.includes('-1')) {
+		//Removes first(duped) overlapping coordinates in subsequent files
+		rawCoordinates = rawCoordinates.slice(2, rawCoordinates.length)
+	}
+
+	let GROUPED_COORDS = []
+	GROUPED_COORDS.push(rawCoordinates)
+	let GROUPED_DUR = []
+	GROUPED_DUR.push(rawDuration)
+	let GROUPED_DIST = []
+	GROUPED_DIST.push(rawDistance)
+
+	combineMetrics(GROUPED_COORDS, GROUPED_DUR, GROUPED_DIST)
+
+	replaceData(pub)
+}
+
+function loadGroupedFiles(fileGroup) {
+	let nameFiles = fileGroup
+	nameFiles = nameFiles.split()
+	nameFiles.map(loadSingleFile)
+	// let pubName = file.split('-')[2]
+	// let filePath = `${IN_PATH}${file}`
+	// let raw = fs.readFileSync(`${filePath}`, 'utf-8')
+	// let parsedData = JSON.parse(raw)
+	// let rawCoordinates = parsedData.routes[0].geometry.coordinates
+	// let rawDuration = parsedData.routes[0].duration
+	// let rawDistance = parsedData.routes[0].distance
+	//
+	// if (!filePath.includes('-1')) {
+	// 	//Removes first(duped) overlapping coordinates in subsequent files
+	// 	rawCoordinates = rawCoordinates.slice(2, rawCoordinates.length)
+	// }
+	//
+	// GROUPED_COORDS.push(rawCoordinates)
+	// GROUPED_DUR.push(rawDuration)
+	// GROUPED_DIST.push(rawDistance)
+	//
+	// combineMetrics()
+	//
+	// replaceData(pubName)
+}
+
+function combineFiles(strippedName) {
+	let pub = strippedName.stripped
+
+	GROUPED_FILES = fs.readdirSync(IN_PATH).filter(d => d.includes(pub))
+
+	GROUPED_FILES.map(loadGroupedFiles)
+
+}
+
 function init() {
 	const files = fs.readdirSync(IN_PATH).filter(d => d.includes('.txt'))
 
 	files.map(stripFilename);
 	PUB_NAMES.map(combineFiles)
-	combineMetrics()
-
-	PUB_NAMES.map(replaceData)
 }
 
 init();
